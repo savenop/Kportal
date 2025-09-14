97f616143011580d5992879e1ce464f43d6e5e10
devdrift7 <devanshchaudhary.work@gmail.com>
2025-09-14 17:02:36 +0530

    Initial

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "kportal-cache-v1";
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json"
+];
+
+// Install
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+// Fetch
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
