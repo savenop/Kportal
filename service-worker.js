46cb0af4ea3c4babff2e2ed8342b17de869f721c
devdrift7 <devanshchaudhary.work@gmail.com>
2025-09-15 22:51:54 +0530

    fixed bugs

--- service-worker.js
+++ service-worker.js
@@ -4,17 +4,20 @@ const urlsToCache = [
   "/index.html",
   "/styles.css",
   "/script.js",
+  "/TT/mon.html",
+  "/TT/tue.html",
+  "/TT/main.css",
   "/manifest.json"
 ];
 
-// Install
+// resposible to install
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
+// it will fetch 
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
